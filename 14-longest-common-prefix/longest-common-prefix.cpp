438672bbb2b72c88856e2e11e563b5928fbd50a9
Aditya Rajput <aditya08.r2006@gmail.com>
2026-09-14 15:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (7.87%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string finalanswer="";
-        int j=0;
-        int size= strs.size()-1;
-        int tempsize;
-        if(strs.size()==0)
-        {
-            return finalanswer;
-        }
-        if(strs.size()==1)
-        {
-            finalanswer= strs[0];
-            return finalanswer;
-        }
-        sort(strs.begin(),strs.end());
-        if(strs[0].size()>strs[size].size())
-        {
-            tempsize= strs[size].size();
-        }
-        else
-        {
-            tempsize= strs[0].size();
-        }
-            while(j<tempsize && strs[0][j]== strs[size][j])
-            {
-                if(strs[0]=="")
-                {
-                    return finalanswer;
-                }
-                finalanswer= finalanswer+strs[0][j];
-                j=j+1;
-            }
+       if(strs.size()==0)
+       {
+        return "";
+       }
+       if(strs.size()==1)
+       {
+        return strs[0];
+       }
+       sort(strs.begin(), strs.end());
+       int i= 0;
+       int size= strs.size()-1;
+       string finalanswer="";
+       if(strs[0]==strs[size])
+       {
+        finalanswer= strs[0];
         return finalanswer;
+       }
+       while(strs[0][i]==strs[size][i])
+       {
+            finalanswer= finalanswer + strs[0][i];
+            i= i+1;
+       }
+       return finalanswer;
     }
 };
